5cd66689b6fe901e37ae0bf636f874fabc20f28d
PedroDionisioDosSantosLoch <pedrodionisiosantosloch@gmail.com>
2026-07-02 10:33:36 -0300

    classesController feito

--- Back/config/database.js
+++ Back/config/database.js
@@ -1,1 +1,13 @@
+const { Sequelize } = require('sequelize');
 
+const sequelize = new Sequelize(
+  'database',
+  'root',
+  'senai',
+  {
+    host: 'localhost',
+    dialect: 'mysql'
+  }
+);
+
+module.exports = sequelize;
