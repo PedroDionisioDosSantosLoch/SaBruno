a7963940a9d404543e952ee7ed143cdafc43b1bb
Gus <runjagoat@gmail.com>
2026-07-02 10:38:28 -0300

    Criação de tabelas no models

---
+++ Back/models/Enrollments.js
@@ -0,0 +1,32 @@
+import {DataTypes} from 'sequelize';
+
+const Enrollment=database.define('Enrollment',{
+    id:{
+        type: DataTypes.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+        allowNull:false
+    },
+    name:{
+        type: DataTypes.STRING,
+        allowNull:false
+    },
+    endereco:{
+        type: DataTypes.STRING,
+        allowNull:false
+    },
+    sala:{
+        type: DataTypes.INTEGER,
+        allowNull:false
+    },
+    email:{
+        type: DataTypes.STRING,
+        allowNull:false
+    },
+    senha:{
+        type: DataTypes.STRING,
+        allowNull:false
+    }
+})
+
+module.exports=Enrollment;
