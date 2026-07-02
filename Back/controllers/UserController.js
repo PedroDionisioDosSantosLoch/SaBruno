50b3f0ec29fca52a100b6df0f7d722a7fe92bcbc
PedroDionisioDosSantosLoch <pedrodionisiosantosloch@gmail.com>
2026-07-02 10:48:53 -0300

    UserController criado

---
+++ Back/controllers/UserController.js
@@ -0,0 +1,51 @@
+const User = require('../models/User');
+
+const getAll = async (req, res) => {
+    const data = await User.findAll();
+    res.json(data);
+};
+
+const getById = async (req, res) => {
+    const data = await User.findByPk(req.params.id);
+
+    if (!data) {
+        return res.status(404).json({ message: 'Registro não encontrado' });
+    }
+
+    res.json(data);
+};
+
+const create = async (req, res) => {
+    const novoRegistro = await User.create(req.body);
+    res.status(201).json(novoRegistro);
+};
+
+const update = async (req, res) => {
+    const data = await User.findByPk(req.params.id);
+
+    if (!data) {
+        return res.status(404).json({ message: 'Registro não encontrado' });
+    }
+
+    await data.update(req.body);
+    res.json(data);
+};
+
+const remove = async (req, res) => {
+    const data = await User.findByPk(req.params.id);
+
+    if (!data) {
+        return res.status(404).json({ message: 'Registro não encontrado' });
+    }
+
+    await data.destroy();
+    res.json({ message: 'Registro removido com sucesso' });
+};
+
+module.exports = {
+    getAll,
+    getById,
+    create,
+    update,
+    remove
+};
