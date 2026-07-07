2b24c8b028d72e22c396a0175d03aa7140bebc33
Erick Zanatta <107509687+ErickZanatta@users.noreply.github.com>
2026-07-07 09:03:45 -0300

    added routes to login and dashboard and install react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import DashBoard from './pages/DashBoard'
 import Login from './pages/Login'
 
 function App() {
   return (
-    <div>
-      <DashBoard />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<DashBoard />} />
+        <Route path="*" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
