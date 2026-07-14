42184c7814779a167802f5de8b9fe5e45e222c77
Gus <runjagoat@gmail.com>
2026-07-13 21:28:21 -0300

    ajeitando import do user

--- Back/models/User.js
+++ Back/models/User.js
@@ -1,5 +1,5 @@
-import { DataTypes } from require('sequelize');
-import sequelize from require('../config/database.js');
+import { DataTypes } from 'sequelize';
+import sequelize from '../config/database.js';
 
 const User = sequelize.define('User', {
     id: {
