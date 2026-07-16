38a5fe1f76efc5834b6e9da47be8336cdcd8f249
Gus <runjagoat@gmail.com>
2026-07-15 22:03:30 -0300

    porra errei o import por causa de letra Maiuscula

--- Back/routes/StudentsRoutes.js
+++ Back/routes/StudentsRoutes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import Router from  'express';
 import { validate, studentSchema } from '../middleware/validation.js';
 import StudentsController from '../controllers/StudentsController.js';
-import authMiddleware from '../middleware/AuthMiddleware.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = Router();
 
@@ -10,7 +10,6 @@ const router = Router();
 router.post('/', authMiddleware, validate(studentSchema), StudentsController.create);
 router.get('/', StudentsController.getAllStudents);
 router.get('/:id', StudentsController.getStudentById);
-router.post('/', StudentsController.createStudent);
 router.put('/:id', StudentsController.updateStudent);
 router.delete('/:id', StudentsController.deleteStudent);
 
